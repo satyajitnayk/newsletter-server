bb0ac0c3dfa3dfde4066d221cd4b3f5b278247c0
satyajit nayak <satyajitn15@gmail.com>
2025-05-17 11:26:11 +0530

    add basic code setup tests, CI/CD, health_check end point (#2)
    - setup health_check endpoint
    - add test case for the end point
    - put server route logic in lib so we can easily test it out
    - select random available PORT when running test(using port no 0)
    - refactor(workflow): restrict push to main and limit PR triggers to relevant events

---
+++ tests/health_check.rs
@@ -0,0 +1,27 @@
+//! tests/health_check.rs
+
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    // Port 0 is special-cased at the OS level: trying to bind port 0 will trigger an OS scan
+    // for an availableport which will then be bound to the application.
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = newsletter_server::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
